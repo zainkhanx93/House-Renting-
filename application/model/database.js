8b3ca1b55f277717c81070c9515811ed312362cb
gemlagman <gemlagman96@gmail.com>
2019-12-06 19:17:38 -0800

    linked db and added routes for inserting user, account, listing, and booking

---
+++ application/model/database.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "mydb",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+pool.getConnection(err => {
+  if (err) throw err;
+  console.log("My database is connected!");
+});
+
+module.exports = pool;
