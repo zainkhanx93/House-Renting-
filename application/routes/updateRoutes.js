204ee4135002de9da42fdaf0c5057cf5fe6cd320
Douglas Hebel <hebeldouglas@gmail.com>
2019-12-10 18:47:32 -0800

    UpdateRoutes Added

--- application/routes/updateRoutes.js
+++ application/routes/updateRoutes.js
@@ -10,8 +10,38 @@ router.post("/updateListingOneToOne", (req, res) => {
   var updateListingOneToOne = `UPDATE Listing L, Amenities A SET A.pool = ${pool}, A.parking = ${parking} WHERE L.lid = ${lid} AND L.lid = A.lid;`;
   db.execute(updateListingOneToOne, (err, result) => {
     if (err) console.log(err);
-    res.send("Sucess");
+    res.send("Success");
   });
 });
 
+router.post("/updateListingOneToMany", (req, res) => {
+    var { body } = req;
+    var { name, email, username } = body;
+
+    var updateListingOneToMany = `UPDATE User U, Account A SET U.email = ${email}, A.username = ${username} WHERE U.name = ${name} AND U.uid = A.uid;`;
+    db.execute(updateListingOneToMany, (err, result) => {
+        if (err) console.log(err);
+        res.send("Success");
+    });
+});
+ router.post("/updateListingManyToMany", (req, res) => {
+     var { body } = req;
+     var { startdate, noofguests, renteeId } = body;
+
+     var updateListingManyToMany = `UPDATE Rentee_Booking R, Booking B SET B.startdate = ${startdate}, B.noofguests = ${noofguests} WHERE R.renteeId = ${renteeId};`;
+     db.execute(updateListingManyToMany, (err, result) => {
+         if (err) console.log(err);
+         res.send("Success");
+     });
+ });
+ router.post("/updateListingManyToOne", (req, res) => {
+     var { body } = req;
+     var { noofguests, lid} = body;
+
+     var updateListingManyToOne = `UPDATE Listing L, Booking B SET B.noofguests = ${noofguests} WHERE L.lid = ${lid};`;
+     db.execute(updateListingManyToOnem, (err, result) => {
+         if (err) console.log(err);
+         res.end("Success");
+     });
+ });
 module.exports = router;
