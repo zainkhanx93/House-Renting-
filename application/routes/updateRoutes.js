bae9c27e46f1c5adc0080c29bd71c88fa3efe825
gemlagman <gemlagman96@gmail.com>
2019-12-10 20:05:48 -0800

    added update routes

--- application/routes/updateRoutes.js
+++ application/routes/updateRoutes.js
@@ -15,33 +15,35 @@ router.post("/updateListingOneToOne", (req, res) => {
 });
 
 router.post("/updateListingOneToMany", (req, res) => {
-    var { body } = req;
-    var { name, email, username } = body;
+  var { body } = req;
+  var { name, email, username } = body;
 
-    var updateListingOneToMany = `UPDATE User U, Account A SET U.email = ${email}, A.username = ${username} WHERE U.name = ${name} AND U.uid = A.uid;`;
-    db.execute(updateListingOneToMany, (err, result) => {
-        if (err) console.log(err);
-        res.send("Success");
-    });
+  var updateListingOneToMany = `UPDATE User U, Account A SET U.email = '${email}', A.username = '${username}' WHERE U.name = '${name}' AND U.uid = A.uid;`;
+  db.execute(updateListingOneToMany, (err, result) => {
+    if (err) console.log(err);
+    res.send("Success");
+  });
 });
- router.post("/updateListingManyToMany", (req, res) => {
-     var { body } = req;
-     var { startdate, noofguests, renteeId } = body;
 
-     var updateListingManyToMany = `UPDATE Rentee_Booking R, Booking B SET B.startdate = ${startdate}, B.noofguests = ${noofguests} WHERE R.renteeId = ${renteeId};`;
-     db.execute(updateListingManyToMany, (err, result) => {
-         if (err) console.log(err);
-         res.send("Success");
-     });
- });
- router.post("/updateListingManyToOne", (req, res) => {
-     var { body } = req;
-     var { noofguests, lid} = body;
+router.post("/updateListingManyToMany", (req, res) => {
+  var { body } = req;
+  var { startdate, noofguests, renteeId } = body;
 
-     var updateListingManyToOne = `UPDATE Listing L, Booking B SET B.noofguests = ${noofguests} WHERE L.lid = ${lid};`;
-     db.execute(updateListingManyToOnem, (err, result) => {
-         if (err) console.log(err);
-         res.end("Success");
-     });
- });
+  var updateListingManyToMany = `UPDATE Rentee_Booking R, Booking B SET B.startdate = '${startdate}', B.noofguests = ${noofguests} WHERE R.renteeId = ${renteeId};`;
+  db.execute(updateListingManyToMany, (err, result) => {
+    if (err) console.log(err);
+    res.send("Success");
+  });
+});
+
+router.post("/updateListingManyToOne", (req, res) => {
+  var { body } = req;
+  var { noofguests, lid } = body;
+
+  var updateListingManyToOne = `UPDATE Listing L, Booking B SET B.noofguests = ${noofguests} WHERE L.lid = ${lid} AND L.lid = B.lid;`;
+  db.execute(updateListingManyToOne, (err, result) => {
+    if (err) console.log(err);
+    res.end("Success");
+  });
+});
 module.exports = router;
